42166a8b2351487dd9f4f8c653febccc7cde1dca
gface <89259112970@mail.ru>
2025-05-20 12:44:21 +0300

    add: BookingRepository

---
+++ src/main/java/com/coworking/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.coworking.repository;
+
+import com.coworking.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
