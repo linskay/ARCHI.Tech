11e74ac282f5e51d425e058e3f30537be6f1220b
Lins <alina.cheremiskina@gmail.com>
2025-05-22 19:26:27 +0300

    исправлены зависимости, починила ламбук, джиюнит винтаж+ баг: после коммита аунтификации 403

--- src/test/java/com/arhi/tech/ApplicationTests.java
+++ src/test/java/com/arhi/tech/ApplicationTests.java
@@ -1,9 +1,10 @@
 package com.arhi.tech;
 
+import com.coworking.CoworkingSpaceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CoworkingSpaceApplication.class)
 class ApplicationTests {
 
     @Test
